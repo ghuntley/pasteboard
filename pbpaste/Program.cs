847c7ebb5ab92caac62169051daa643b288e9a40
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-01-19 15:08:17 +1100

    initial commit

---
+++ pbpaste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pbpaste
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var pasteboard = Clipboard.GetText();
+
+            Console.WriteLine(pasteboard);
+        }
+    }
+}
