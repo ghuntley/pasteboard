f60cd03283e7831756bc5ddc3fcdb4cd01542989
Restyled.io <commits@restyled.io>
2020-04-20 10:49:17 +0000

    Restyled by clang-format

--- pbpaste/Program.cs
+++ pbpaste/Program.cs
@@ -5,19 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace pbpaste
-{
-class Program
-{
-    [STAThread]
-    static void Main(string[] args)
-    {
-        var pasteboard = Clipboard.GetText();
-        if (args.Length > 0 && "--lf".Equals(args[0]))
-        {
-            pasteboard = pasteboard.Replace("\r\n", "\n");
-        }
-        Console.Write(pasteboard);
+namespace pbpaste {
+class Program {
+  [STAThread]
+  static void
+  Main(string[] args) {
+    var pasteboard = Clipboard.GetText();
+    if (args.Length > 0 && "--lf".Equals(args[0])) {
+      pasteboard = pasteboard.Replace("\r\n", "\n");
     }
+    Console.Write(pasteboard);
+  }
 }
 }
